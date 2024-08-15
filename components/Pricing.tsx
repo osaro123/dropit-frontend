365d0a630f9b7668b485dda5c74344eeea46e69a
osaro123 <osarolawani983@gmail.com>
2024-08-16 00:56:39 +0100

    initial commit

--- components/Pricing.tsx
+++ components/Pricing.tsx
@@ -38,8 +38,8 @@ const Pricing = () => {
         <h3 className='text-center font-medium text-3xl mb-4' id='prcing'>Choose Your Plan</h3>
         <p className='text-center opacity-80'>We provide a very simple and transparent pricing model</p>
         <div className='py-16 grid grid-cols-2 gap-8 w-[75%] mx-auto'>
-            {pricingData.map((pricing) => (
-                <div className='border-[1px] border-black p-8 rounded-md'>
+            {pricingData.map((pricing,i) => (
+                <div key={i} className='border-[1px] border-black p-8 rounded-md'>
                     <h3>{pricing.plan}</h3>
                     <div className='flex items-center py-4'>
                         <p className='text-3xl font-bold'>{`$${pricing.amount}`}</p>
@@ -47,8 +47,8 @@ const Pricing = () => {
                     </div>
                     <p className='pb-4'>{pricing.description}</p>
                     <ul>
-                        {pricing.perks.map(({description,available}) => (
-                            <li className='pb-2 flex items-center gap-2'>
+                        {pricing.perks.map(({description,available},i) => (
+                            <li key={i} className='pb-2 flex items-center gap-2'>
                                 {available ? <FaCircleCheck /> : <IoCloseCircle size={20}/>}
                                 <p>{description}</p>
                             </li>
