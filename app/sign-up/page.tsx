0db3e28f0074994ba51ffa28fae9a35a3055a9df
osaro123 <osarolawani983@gmail.com>
2024-08-16 02:19:52 +0100

    Add sign up page

--- app/sign-up/page.tsx
+++ app/sign-up/page.tsx
@@ -1,24 +1,39 @@
-import React from 'react'
+"use client"
+
+import { Eye, EyeOff } from 'lucide-react'
+import React, { useState } from 'react'
 
 const SignUp = () => {
+    const [showPassword,setShowPassword] = useState(false)
   return (
-    <div className='h-screen'>
-        <h3 className='font-bold'>dropIt</h3>
-        <div>
-            <form>
-                <div>
-                    <label htmlFor="email">Email Address</label>
-                    <input type="text" id='email'/>
+    <div className='w-[90%] h-screen mx-auto'>
+        <h3 className='font-bold py-4'>dropIt</h3>
+        <div className='flex items-center h-[calc(100vh-4rem)] max-w-md w-[90%] mx-auto'>
+            <form className='w-full'>
+                <h1 className='text-2xl pb-2 font-bold'>Unlock Seamless Sharing</h1>
+                <p className='opacity-80 pb-6'>Begin your journey to effortless file and text sharing across all your devices.</p>
+                <div className='flex flex-col mb-4'>
+                    <label htmlFor="email" className='pb-2 font-medium'>Email Address</label>
+                    <input type="email" id='email' className='border-[1px] border-black h-12 px-2 py-3 rounded-md'/>
                 </div>
-                <div>
-                    <label htmlFor="password">Password</label>
-                    <input type="text" id='password'/>
+                <div className='flex flex-col mb-4'>
+                    <label htmlFor="username" className='pb-2 font-medium'>Username</label>
+                    <input type="text" id='username' className='border-[1px] border-black h-12 px-2 py-3 rounded-md'/>
                 </div>
-                <div>
-                    <div></div>
-                    <div></div>
+                <div className='flex flex-col'>
+                    <label htmlFor="password" className='py-2 font-medium'>Password</label>
+                    <div className='flex items-center gap-1 w-full h-12'>
+                        <input type={showPassword ? "text" : "password"} id='password' className='border-[1px] border-black px-2 py-3 rounded-l-md w-[90%]'/>
+                        <button 
+                            type='button'
+                            className='w-[10%] h-12 bg-black text-white flex items-center justify-center rounded-r-md'
+                            onClick={() => setShowPassword(!showPassword)}
+                        >
+                            {showPassword ? <EyeOff /> : <Eye />}
+                        </button>
+                    </div>
                 </div>
-                <button>Create Account</button>
+                <button className='text-center bg-black text-white w-full mt-4 py-4 rounded-full'>Create Account</button>
             </form>
         </div>
     </div>
